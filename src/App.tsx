3d766f1a287165c72e088d62d8066ed1aa7153db
Michael Churilov <michael.churilov@gmail.com>
2021-04-12 22:42:01 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import { Timer } from './classes/timer'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, setState] = useState<{ timer: Timer }>({
+        timer: new Timer(),
+    })
+
+    const startTimer = () => {
+        state.timer.start(setState)
+        setState({ timer: state.timer })
+    }
+
+    const stopTimer = () => {
+        state.timer.stop()
+        setState({ timer: state.timer })
+    }
+
+    return (
+        <>
+            <h1>{state.timer.timeRemaining.toFixed(0)}</h1>
+            <button onClick={startTimer}>Start timer!</button>
+            <button onClick={stopTimer}>Stop timer!</button>
+        </>
+    )
 }
 
-export default App;
+export default App
