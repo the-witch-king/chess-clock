6bd770906837de17b7ecf88bd807cce8d025a4e7
Michael Churilov <michael.churilov@gmail.com>
2021-04-19 19:54:54 -0400

    Simplification continues

--- src/components/timer-view.tsx
+++ src/components/timer-view.tsx
@@ -30,63 +30,6 @@ const initializeTimer = (allotedTime: number, id: TimerId): Timer => ({
 
 const isTimerRunning = (timer: Timer, id: Symbol): boolean => timer.id === id
 
-export const toggleTimers = (
-    [firstTimer, secondTimer]: TimerState,
-    setState: Dispatch<SetStateAction<TimerState>>,
-    { runningTimerId, intervalReference }: IntervalState,
-    setIntervalReference: Dispatch<SetStateAction<IntervalState>>,
-    increaseAmount: number
-): void => {
-    // Game's done!
-    if (firstTimer.timeRemaining <= 0 || secondTimer.timeRemaining <= 0) return
-
-    let timerToStart: Timer
-    let timerToStop: Timer
-
-    if (isTimerRunning(firstTimer, runningTimerId)) {
-        timerToStart = { ...secondTimer, lastTick: new Date() }
-        timerToStop = firstTimer
-    } else {
-        timerToStart = { ...firstTimer, lastTick: new Date() }
-        timerToStop = secondTimer
-    }
-
-    if (timerToStop.timeRemaining !== timerToStart.timeRemaining) {
-        timerToStop.timeRemaining += increaseAmount
-    }
-
-    if (!!intervalReference) clearInterval(intervalReference)
-
-    const newIntervalReference = setInterval(() => {
-        const now = new Date()
-        const delta =
-            Math.floor(now.getTime() - timerToStart.lastTick.getTime()) / 1000
-        const newRemainingTime = timerToStart.timeRemaining - delta
-
-        setState(() => [
-            {
-                timeRemaining: newRemainingTime,
-                lastTick: now,
-                id: timerToStart.id,
-            },
-            {
-                timeRemaining: timerToStop.timeRemaining,
-                lastTick: new Date(),
-                id: timerToStop.id,
-            },
-        ])
-
-        if (newRemainingTime <= 0) {
-            clearInterval(newIntervalReference)
-        }
-    }, TICK_INTERVAL)
-
-    setIntervalReference({
-        intervalReference: newIntervalReference,
-        runningTimerId: timerToStart.id,
-    })
-}
-
 export const TimerView = ({
     startingTime,
     toggleViews,
@@ -101,32 +44,74 @@ export const TimerView = ({
         initializeTimer(startingTime, SECOND_TIMER),
     ])
 
-    console.log('State: ', state)
-
-    const [intervalReference, setIntervalReference] = useState<IntervalState>({
+    const [intervalState, setIntervalReference] = useState<IntervalState>({
         runningTimerId: SECOND_TIMER,
         intervalReference: null,
     })
 
+    const toggleTimers = (): void => {
+        const [firstTimer, secondTimer] = state
+        const { runningTimerId, intervalReference } = intervalState
+
+        // Game's done!
+        if (firstTimer.timeRemaining <= 0 || secondTimer.timeRemaining <= 0)
+            return
+
+        let timerToStart: Timer
+        let timerToStop: Timer
+
+        if (isTimerRunning(firstTimer, runningTimerId)) {
+            timerToStart = { ...secondTimer, lastTick: new Date() }
+            timerToStop = firstTimer
+        } else {
+            timerToStart = { ...firstTimer, lastTick: new Date() }
+            timerToStop = secondTimer
+        }
+
+        if (timerToStop.timeRemaining !== timerToStart.timeRemaining) {
+            timerToStop.timeRemaining += increaseAmount
+        }
+
+        if (!!intervalReference) clearInterval(intervalReference)
+
+        const newIntervalReference = setInterval(() => {
+            const now = new Date()
+            const delta =
+                Math.floor(now.getTime() - timerToStart.lastTick.getTime()) /
+                1000
+            const newRemainingTime = timerToStart.timeRemaining - delta
+
+            setState(() => [
+                {
+                    timeRemaining: newRemainingTime,
+                    lastTick: now,
+                    id: timerToStart.id,
+                },
+                {
+                    timeRemaining: timerToStop.timeRemaining,
+                    lastTick: new Date(),
+                    id: timerToStop.id,
+                },
+            ])
+
+            if (newRemainingTime <= 0) {
+                clearInterval(newIntervalReference)
+            }
+        }, TICK_INTERVAL)
+
+        setIntervalReference({
+            intervalReference: newIntervalReference,
+            runningTimerId: timerToStart.id,
+        })
+    }
+
     const onToggle = (): void => {
-        toggleTimers(
-            state,
-            setState,
-            intervalReference,
-            setIntervalReference,
-            increaseAmount
-        )
+        toggleTimers()
     }
 
     const onKeyDown = (event: React.KeyboardEvent) => {
         if (event.code === SPACE_KEY_CODE) {
-            toggleTimers(
-                state,
-                setState,
-                intervalReference,
-                setIntervalReference,
-                increaseAmount
-            )
+            toggleTimers()
             event.preventDefault()
             event.stopPropagation()
         }
